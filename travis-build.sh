fcf92be9b9318d886eeec367d8360651e077f9a1
Uri Herrera <hello@uriherrera.com>
2021-09-09 20:51:51 -0500

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -4,7 +4,7 @@ apt -qq update
 apt -qq -yy install equivs curl git
 
 ### Install Dependencies
-DEBIAN_FRONTEND=noninteractive apt -yy --no-install-recommends install devscripts lintian build-essential automake autotools-dev keyboard-configuration console-setup
+apt -qq -yy install devscripts lintian build-essential automake autotools-dev
 mk-build-deps -i -t "apt-get --yes" -r
 
 ### Build Deb
